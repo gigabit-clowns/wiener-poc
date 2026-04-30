1ed4b1f44c62bc1a03c12db26fab2a5c46648912
Martin Salinas <ssalinasmartin@gmail.com>
2026-04-30 11:05:50 +0200

    First dummy kernel

--- build.rs
+++ build.rs
@@ -1,4 +1,23 @@
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
 fn main() {
     // This tells Cargo to recompile if we add or modify any CUDA code
-    println!("cargo:rerun-if-changed=cuda_kernels/");
+    println!("cargo:rerun-if-changed=cuda_kernels/wiener.cu");
+
+    // Rust provides a temp folder for build artifacts
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let ptx_path = out_dir.join("wiener.ptx");
+
+    // Invoke nvcc to generate PTX
+    let status = Command::new("nvcc")
+        .arg("-ptx") // Generate intermediate PTX code
+        .arg("cuda_kernels/wiener.cu")
+        .arg("-o")
+        .arg(&ptx_path)
+        .status()
+        .expect("Error running nvcc. Is CUDA in PATH?");
+
+    assert!(status.success(), "Error compiling CUDA kernel. Check syntax in wiener.cu");
 }
--- cuda_kernels/weiner.cu
+++ cuda_kernels/weiner.cu
@@ -1,1 +1,7 @@
-// Dummy comment
+extern "C" __global__ void wiener_dummy(float* out, const float* in, int num_elements) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < num_elements) {
+        // Dummy operation to validate kernel running
+        out[idx] = in[idx] * 0.1f;
+    }
+}
